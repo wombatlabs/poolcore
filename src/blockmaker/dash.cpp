68f79477e35ec8257a8f25dd6f45aa09d82c6d76
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2025-04-24 17:26:36 -0700

    Fix

--- src/blockmaker/dash.cpp
+++ src/blockmaker/dash.cpp
@@ -1,34 +1,36 @@
 #include "blockmaker/dash.h"
 #include "blockmaker/serialize.h"
+#include "p2putils/xmstream.h"
 
 namespace BTC {
 
-template <>
-void Io<DASH::Proto::Transaction>::serialize(xmstream &dst, const DASH::Proto::Transaction &data, bool /*serializeWitness*/) {
-    BTC::serialize(dst, data.version);
-    BTC::serialize(dst, data.vin);
-    BTC::serialize(dst, data.vout);
-    BTC::serialize(dst, data.lockTime);
+template<>
+struct Io<DASH::Proto::Transaction> {
+    static void serialize(xmstream &dst, const DASH::Proto::Transaction &data) {
+        BTC::serialize(dst, data.version);
+        BTC::serialize(dst, data.vin);
+        BTC::serialize(dst, data.vout);
+        BTC::serialize(dst, data.lockTime);
 
-    if (!data.vExtraPayload.empty()) {
-        BTC::serializeVarSize(dst, data.vExtraPayload.size());
-        dst.write(data.vExtraPayload.data(), data.vExtraPayload.size());
+        if (!data.vExtraPayload.empty()) {
+            BTC::serializeVarSize(dst, data.vExtraPayload.size());
+            dst.write(data.vExtraPayload.data(), data.vExtraPayload.size());
+        }
     }
-}
 
-template <>
-void Io<DASH::Proto::Transaction>::unserialize(xmstream &src, DASH::Proto::Transaction &data) {
-    BTC::unserialize(src, data.version);
-    BTC::unserialize(src, data.vin);
-    BTC::unserialize(src, data.vout);
-    BTC::unserialize(src, data.lockTime);
+    static void unserialize(xmstream &src, DASH::Proto::Transaction &data) {
+        BTC::unserialize(src, data.version);
+        BTC::unserialize(src, data.vin);
+        BTC::unserialize(src, data.vout);
+        BTC::unserialize(src, data.lockTime);
 
-    if (src.remaining()) {
-        uint64_t payloadSize;
-        BTC::unserializeVarSize(src, payloadSize);
-        data.vExtraPayload.resize(payloadSize);
-        src.read(data.vExtraPayload.data(), payloadSize);
+        if (src.remaining()) {
+            uint64_t payloadSize;
+            BTC::unserializeVarSize(src, payloadSize);
+            data.vExtraPayload.resize(payloadSize);
+            src.read(data.vExtraPayload.data(), payloadSize);
+        }
     }
-}
+};
 
 } // namespace BTC

4750145c944f6be24f4247c6c348c6e9a26453dc
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2025-05-04 13:45:15 -0700

    Update

--- src/include/blockmaker/dash.h
+++ src/include/blockmaker/dash.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <cstdint>
 #include <vector>
-#include "btcLike.h"
-#include "serialize.h"
+
 #include "btc.h"
+#include "serialize.h"
 
 namespace DASH {
 namespace Proto {
@@ -23,3 +23,42 @@ struct Transaction {
 } // namespace Proto
 } // namespace DASH
 
+namespace DASH {
+
+using Stratum = WorkTy<
+    DASH::Proto,
+    BTC::Stratum::HeaderBuilder,
+    BTC::Stratum::CoinbaseBuilder,
+    BTC::Stratum::Notify,
+    BTC::Stratum::Prepare
+>;
+
+static Stratum::Work *newPrimaryWork(int64_t stratumId,
+                                     CBlockTemplate &blockTemplate,
+                                     const CMiningConfig &miningCfg,
+                                     PoolBackend *backend,
+                                     size_t backendId,
+                                     std::string &error) {
+    if (blockTemplate.WorkType != EWorkBitcoin) {
+        error = "invalid work type";
+        return nullptr;
+    }
+
+    std::unique_ptr<Stratum::Work> work(new Stratum::Work(stratumId,
+                                                          blockTemplate.UniqueWorkId,
+                                                          backend,
+                                                          backendId,
+                                                          miningCfg));
+
+    return work->loadFromTemplate(blockTemplate, error) ? work.release() : nullptr;
+}
+
+struct X {
+  using Proto = DASH::Proto;
+  using Stratum = DASH::Stratum;
+
+  template<typename T> static inline void serialize(xmstream &src, const T &data) { Io<T>::serialize(src, data); }
+  template<typename T> static inline void unserialize(xmstream &dst, T &data) { Io<T>::unserialize(dst, data); }
+};
+
+} // namespace DASH
